c85beb13187800b8f8566d040d3e46686e660d14
asrafayyasi <ayyasiasraf@gmail.com>
2023-02-15 13:17:54 +0700

    Menambahkan Seluruh File Baru

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import './homepage.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyHomePage());
+  }
+}
